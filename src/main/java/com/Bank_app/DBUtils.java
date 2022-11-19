a0dbfd2ed1dfea4a2208857ea84e7edd32a82852
Anirudhgambhir <anirudhgambhir10@gmail.com>
2022-11-19 20:40:06 +0530

    commit

--- src/main/java/com/Bank_app/DBUtils.java
+++ src/main/java/com/Bank_app/DBUtils.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DBUtils {
+    //TESTING GIT COMMIT AMEND
     private static final String DB_URL = "jdbc:mysql://localhost:3306/";
     private static final String USER_NAME = "root";
     private static final String PASSWORD = "NI@ma5678";
