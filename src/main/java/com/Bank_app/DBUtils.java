688e37eb56fdc4986517a7a2f151dbfbf316b5e6
Anirudhgambhir <anirudhgambhir10@gmail.com>
2022-11-19 20:47:38 +0530

    Reverting Amend command testing changes

--- src/main/java/com/Bank_app/DBUtils.java
+++ src/main/java/com/Bank_app/DBUtils.java
@@ -6,9 +6,6 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DBUtils {
-    //TESTING GIT COMMIT AMEND
-    // COMMIT 2
-    // Commit 3
     private static final String DB_URL = "jdbc:mysql://localhost:3306/";
     private static final String USER_NAME = "root";
     private static final String PASSWORD = "NI@ma5678";
